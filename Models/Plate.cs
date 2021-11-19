5623acb2990a3915d3a18a0cda63ec1f423c082a
Hessler Inampuez <hesslernevin@gmail.com>
2021-11-18 22:20:28 -0500

    Agregar archivos de proyecto.

---
+++ Models/Plate.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantWebApplication.Models
+{
+    public class Plate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
